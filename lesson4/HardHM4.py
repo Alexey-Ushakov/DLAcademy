e4cdee6a6615d60e350854cb6f6fc5d6b129fe5f
AlexeyUshakov <alexeyushakov42@gmail.com>
2021-06-22 15:39:05 +0300

    Хард первая и вторая задача решена

--- lesson4/HardHM4.py
+++ lesson4/HardHM4.py
@@ -9,31 +9,79 @@
 # Ввод: -2/3 - -2
 # Вывод: 1 1/3
 # a = input("Введите дроби через пробел: ")
-a = 5/6 + 4/7
-a = a.split()
 
-a[0] = a[0].split("/")
-a[2] = a[2].split("/")
-b = ["1"]
-if len(a[0]) == 1:
-    a[0] = list(a[0])
-    a[0] = a[0] + b
-if len(a[2]) == 1:
-    a[2] = list(a[2])
-    a[2] = a[2] + b
+def func_list_drob(element):
+    """
+    Возвращает список дроби где нулевой и первый элемент числитель и знаменатель целой частиБ второй и третий элемент
+    числитель и знаменатель дробной части
+    :param element: Исходная дробь
+    :return: Список целых чисел
+    """
+    cel_chast = "0"
+    drobn_chast = "0"
+    list_1 = element.split()
+
+    if len(list_1) == 2:
+        cel_chast = list_1[0]
+        drobn_chast = list_1[1]
+    else:
+        if '/' in list_1[0]:
+            drobn_chast = list_1[0]
+        else:
+            cel_chast = list_1[0]
+    list_cel_chast = [cel_chast, "1"]
+
+    if drobn_chast == "0":
+        list_drob_chast = ["0", "1"]
+    else:
+        list_drob_chast = drobn_chast.split("/")
+    list_of_drob = [int(i) for i in list_cel_chast+list_drob_chast]
+    # list_of_drob = list(map(int,list_cel_chast+list_drob_chast))
+    return list_of_drob
+
+def obshai_drob(element):
+    """
+    Возвращает список где нулевой элемент числитель дроби содержащий целую часть и первый элемент знаменатель дроби
+    :param element: исходная дробь с целочисленной частью или без нее
+    :return:
+    """
+    if element[0] < 0:
+        obsh_chislitel = element[0] * element[3] - element[2] * element[1]
+    else:
+        obsh_chislitel = element[0] * element[3] + element[2] * element[1]
+    obch_znamenatel = element[1] * element[3]
+    list_obshai_drob = [obsh_chislitel, obch_znamenatel]
+    return list_obshai_drob
 
 
-if a[1] == "+":
-    obch_chislitel = int(a[0][0]) * int(a[2][1]) + int(a[0][1]) * int(a[2][0])
-    obch_znam = int(a[0][1]) * int(a[2][1])
-    celai = obch_chislitel // obch_znam
-    drobnai = abs(obch_chislitel % obch_znam)
-elif a[1] == "-":
-    obch_chislitel = int(a[0][0]) * int(a[2][1]) - int(a[0][1]) * int(a[2][0])
-    obch_znam = int(a[0][1]) * int(a[2][1])
-    celai = obch_chislitel // obch_znam
-    drobnai = abs(obch_chislitel % obch_znam)
-print("{} {}/{}".format(celai,drobnai,obch_znam ))
+drob = "- 5/6 - 2/8"
+SIGN_PLUS = " + "
+SIGN_MINUS = " - "
+sign = "+"
+if SIGN_PLUS in drob:
+    drob_exp = drob.split(SIGN_PLUS)
+else:
+    drob_exp = drob.split(SIGN_MINUS)
+    sign = "-"
+
+first_drob = drob_exp[0]
+second_drob = drob_exp[1]
+first_drob = func_list_drob(first_drob)
+second_drob = func_list_drob(second_drob)
+first_drob = obshai_drob(first_drob)
+second_drob = obshai_drob(second_drob)
+
+if sign == "-":
+    vicheslenie_chislitl_drobi = first_drob[0] * second_drob[1] - second_drob[0] * first_drob[1]
+else:
+    vicheslenie_chislitl_drobi = first_drob[0] * second_drob[1] + second_drob[0] * first_drob[1]
+vicheslenie_znamenatel_drobi = first_drob[1] * second_drob[1]
+
+print("{}{} {}/{}".format("" if vicheslenie_chislitl_drobi >= 0 else '-',
+                          "" if abs(vicheslenie_chislitl_drobi) // vicheslenie_znamenatel_drobi ==0
+                          else abs(vicheslenie_chislitl_drobi) // vicheslenie_znamenatel_drobi,
+                          abs(vicheslenie_chislitl_drobi) % vicheslenie_znamenatel_drobi,
+                          vicheslenie_znamenatel_drobi))
 
 
 # Задание-2:
@@ -101,3 +149,19 @@ for i in new_w:
 # Подсказка:
 # Чтобы получить список больших букв русского алфавита:
 # print(list(map(chr, range(ord('А'), ord('Я')+1))))
+
+#
+# import os
+# path = os.path.join("data", "fruits.txt")
+# f = open(path, encoding="UTF-8" )
+# b = f.readlines()
+# f.close
+# fruits_А = open(r"E:\PycharmProjects\DlAcademy\lesson4\data\fruits_{}.txt".format({}), "w+", encoding="UTF-8")
+# for i in b:
+#     for item in list(map(chr, range(ord('А'), ord('Я') + 1))):
+#
+#         if item == (i)[0:1:]:
+#             file_name = 'fruits_{}.txt'.format(item)
+#             with open(file_name, 'w+', encoding="UTF-8")) as f:
+#                        f.write(i)
+# f.close
