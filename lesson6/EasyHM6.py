611305b4eb52b769722fa2c8490039fedd77ca3e
AlexeyUshakov <alexeyushakov42@gmail.com>
2021-06-30 12:39:02 +0300

    Урок 6 Легкая домашка готова

--- lesson6/EasyHM6.py
+++ lesson6/EasyHM6.py
@@ -13,23 +13,107 @@ def avg(a, b):
         - float.
     """
     return (a * b) ** 0.5
-
-
-a = float(input("a = "))
-b = float(input("b = "))
-c = avg(a, b)
-print("Среднее геометрическое = {:.2f}".format(c))
+while True:
+    try:
+        a = float(input("a = "))
+        b = float(input("b = "))
+        c = avg(a, b)
+    except ValueError:
+        print("Введите число без посторонних символов и букв")
+    else:
+        print("Среднее геометрическое = {:.2f}".format(c))
+        break
 
 # ПРИМЕЧАНИЕ: Для решения задач 2-4 необходимо познакомиться с модулями os, sys!
 # СМ.: https://pythonworld.ru/moduli/modul-os.html, https://pythonworld.ru/moduli/modul-sys.html
+
+import os
+import sys
 
 # Задача-2:
 # Напишите скрипт, создающий директории dir_1 - dir_9 в папке,
 # из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
+def make_dir(derectory):
+
+    try:
+        if os.path.exists(derectory):
+            raise Exception("Это папка уже есть")
+        else:
+            os.mkdir(derectory)
+    except Exception as e:
+        print(e)
+    else:
+        print("дириктория {} создана".format(derectory))
+
+
+
+
+for i in range(1, 10):
+    derectory = "dir_" + str(i)
+    make_dir(derectory)
+
+print(os.listdir(os.path.dirname(__file__)))
+
+
+def make_del_dir(derectory):
+    try:
+        if not os.path.exists(derectory):
+            raise Exception("Нет такой папки")
+        else:
+            os.rmdir(derectory)
+    except Exception as e:
+        print(e)
+    else:
+        print("дириктория {} удалена".format(derectory))
+
+
+for i in range(1, 10):
+    derectory = "dir_" + str(i)
+    make_del_dir(derectory)
+
 
 # Задача-3:
 # Напишите скрипт, отображающий папки текущей директории.
 
+print(os.listdir(os.path.dirname(__file__)))
+
 # Задача-4:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
+
+import sys
+import os
+import shutil
+from os import path
+
+src = path.realpath(__file__)
+head, tail = path.split(src)
+dst = src + ".bak"
+shutil.copy(src, dst)
+
+print(os.listdir(os.path.dirname(__file__)))
+
+
+
+
+
+
+# destdir = os.path.abspath('destdir')
+# if not os.path.exists(destdir):
+#     os.makedirs(destdir)
+
+# # с использованием системного shell'а
+# os.system('copy %s %s' % (__file__, destdir))
+# # другой вариант - с получением полного имени скрипта через sys.argv[0]
+# os.system('copy %s %s' % (sys.argv[0], destdir))
+#
+# # без shell'а
+# dirname, filename = os.path.split(__file__)
+# content = open(__file__).read()
+# open(os.path.join(destdir, filename), 'w').write(content)
+#
+# from shutil import copyfile, copy
+#
+# copy(__file__, destdir)  # копируем в указанную директорию
+# copyfile(__file__, os.path.join(destdir, filename))  # копируем по полному пути нового расположения
+
