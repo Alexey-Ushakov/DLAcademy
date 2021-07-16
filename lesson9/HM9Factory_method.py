1325800f757ebc45786271aefe7d7e87b3441abc
AlexeyUshakov <alexeyushakov42@gmail.com>
2021-07-16 14:41:27 +0300

    HM9 Factory Bad 3.0

--- lesson9/HM9Factory_method.py
+++ lesson9/HM9Factory_method.py
@@ -1,45 +1,64 @@
-# Без асбстрактного метода? О_о и без наследования
-class Image():
-    def __init__(self, src=None):
+# Метод перенес в продукты
+import abc
+
+
+class Tag(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def get_html(self):
+        pass
+
+
+class Image(Tag):
+    def __init__(self, src):
         self.src = src
 
+    def get_html(self):
+        return "<></{}>".format(self.src)
 
-class Input():
-    def __init__(self, in_type=None):
+
+class Input(Tag):
+    def __init__(self, in_type):
         self.in_type = in_type
 
+    def get_html(self):
+        return "<></{}>".format(self.in_type)
 
-class Text():
-    def __init__(self, text=None):
+class Text(Tag):
+    def __init__(self, text):
         self.text = text
 
+    def get_html(self):
+        return "<></{}>".format(self.text)
 
 
-class Link():
-    def __init__(self, link=None):
+class Link(Tag):
+    def __init__(self, link):
         self.link = link
 
+    def get_html(self):
+        return "<></{}>".format(self.link)
 
+class None_text(Tag):
+    def __init__(self, none_text):
+        self.none_text = none_text
+
+    def get_html(self):
+        return "<></{}>".format(self.none_text)
 
 class Tagfactory:
     def create_tag(self, name):
         if name == 'image' or name == "src":
-            return "img"
+            return Image(name)
         elif name == 'input':
-            return "input"
-        elif name == 'p':
-            return "p"
+            return Input(name)
         elif name == 'a':
-            return "a"
-        elif name == "":
-            return ""
-
-    def get_html(self, name):
-        print("<></{}>".format(name))
-
-
+            return Link(name)
+        elif name == 'p':
+            return Text(name)
+        elif name == '':
+            return None_text(name)
 
 factory = Tagfactory()
-elements = ["image", "input", 'p', 'a', '']
+elements = ["image", "input", 'a', 'p', '', 'src']
 for el in elements:
-    factory.get_html(factory.create_tag(el))
+    print(factory.create_tag(el).get_html())
